97a543622a3ba15bd52ceda01e0114f6409ce2eb
Ian Hadrien <ianhadrien@gmail.com>
2022-02-24 17:49:44 -0300

    Enter

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,78 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<body>
+
+  <div class="main-wrapper">
+
+    <div class="preloader">
+          <div class="lds-ripple">
+              <div class="lds-pos"></div>
+              <div class="lds-pos"></div>
+          </div>
+      </div>
+
+      <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background: url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
+
+          <div class="auth-box">
+              <div>
+
+                  <div class="logo">
+                      <span class="db">
+                          <img alt="thumbnail" 
+                              class="rounded-circle" 
+                              width="100" 
+                              src="../assets/images/users/1.jpg">
+                      </span>
+                      <h5 class="font-medium mb-3">Genelia</h5>
+                  </div>
+
+                          <!-- Form -->
+                  <div class="row">
+                      <div class="col-12">
+                              
+                          <form class="form-horizontal mt-3" action="index.html">
+                              <div class="form-group row">
+                                  <div class="col-12">
+                                      <input class="form-control form-control-lg" 
+                                              type="password" 
+                                              required="" 
+                                              placeholder="Password">
+                                  </div>
+                              </div>
+
+                              <div class="form-group text-center">
+                                  <div class="col-xs-12 pb-3">
+                                      <button class="btn btn-block btn-lg btn-info" type="submit">LOGIN</button>
+                                  </div>
+                              </div>
+                          </form>
+
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+                  
+      </div>
+
   </div>
+  
+  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+      <!-- Bootstrap tether Core JavaScript -->
+  <script src="../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+  <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+      <!-- ============================================================== -->
+      <!-- This page plugin js -->
+      <!-- ============================================================== -->
+  <script>
+    $('[data-toggle="tooltip"]').tooltip();
+    $(".preloader").fadeOut();
+  </script>
+
+</body>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    name: 'HelloWorld'
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
