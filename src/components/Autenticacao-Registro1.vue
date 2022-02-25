d9f4994eef7569f6e5e21ee2dab4dd064ff619ca
Ian Hadrien <ianhadrien@gmail.com>
2022-02-24 22:58:09 -0300

    Enter

---
+++ src/components/Autenticacao-Registro1.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="main-wrapper">
+        <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+        <div class="preloader">
+            <div class="lds-ripple">
+                <div class="lds-pos"></div>
+                <div class="lds-pos"></div>
+            </div>
+        </div>
+        
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" 
+        style="background:url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
+            <div class="auth-box">
+                <div>
+                    <div class="logo">
+                        <span class="db"><img src="../assets/images/logos/logo-icon.png" alt="logo" /></span>
+                        <h5 class="font-medium mb-3">Sign Up to Admin</h5>
+                    </div>
+                    <!-- Form -->
+                    <div class="row">
+                        <div class="col-12">
+                            <form class="form-horizontal mt-3" action="index.html">
+                                <div class="form-group row ">
+                                    <div class="col-12 ">
+                                        <input class="form-control form-control-lg" type="text" required=" " placeholder="Name">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-12 ">
+                                        <input class="form-control form-control-lg" type="text" required=" " placeholder="Email">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-12 ">
+                                        <input class="form-control form-control-lg" type="password" required=" " placeholder="Password">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-12 ">
+                                        <input class="form-control form-control-lg" type="password" required=" " placeholder="Confirm Password">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-md-12 ">
+                                        <div class="custom-control custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                            <label class="custom-control-label" for="customCheck1">I agree to all <a href="javascript:void(0)">Terms</a></label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group text-center ">
+                                    <div class="col-xs-12 pb-3 ">
+                                        <button class="btn btn-block btn-lg btn-info " type="submit ">SIGN UP</button>
+                                    </div>
+                                </div>
+                                <div class="form-group mb-0 mt-2 ">
+                                    <div class="col-sm-12 text-center ">
+                                        Already have an account? <a href="authentication-login1.html " class="text-info ml-1 "><b>Sign In</b></a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Registro_one'
+}
+</script>
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,30 +3,23 @@
 
   <div class="main-wrapper">
 
-    <div class="preloader">
-          <div class="lds-ripple">
-              <div class="lds-pos"></div>
-              <div class="lds-pos"></div>
-          </div>
-      </div>
+    <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" 
+          style="background: url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
 
-      <div class="auth-wrapper d-flex no-block justify-content-center align-items-center" style="background: url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
+      <div class="auth-box">
 
-          <div class="auth-box">
-              <div>
-
-                  <div class="logo">
-                      <span class="db">
-                          <img alt="thumbnail" 
+              <div class="logo">
+                  <span class="db">
+                      <img alt="thumbnail" 
                               class="rounded-circle" 
                               width="100" 
                               src="../assets/images/users/1.jpg">
                       </span>
                       <h5 class="font-medium mb-3">Genelia</h5>
-                  </div>
+              </div>
 
-                          <!-- Form -->
-                  <div class="row">
+              <!-- Form -->
+              <div class="row">
                       <div class="col-12">
                               
                           <form class="form-horizontal mt-3" action="index.html">
@@ -47,32 +40,24 @@
                           </form>
 
                       </div>
-                  </div>
-
               </div>
-          </div>
-                  
       </div>
+                  
+    </div>
 
   </div>
-  
-  <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
-      <!-- Bootstrap tether Core JavaScript -->
-  <script src="../assets/libs/popper.js/dist/umd/popper.min.js"></script>
-  <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
-      <!-- ============================================================== -->
-      <!-- This page plugin js -->
-      <!-- ============================================================== -->
-  <script>
-    $('[data-toggle="tooltip"]').tooltip();
-    $(".preloader").fadeOut();
-  </script>
 
 </body>
 </template>
 
 <script>
 export default {
-    name: 'HelloWorld'
-}
+  name: 'HelloWorld',
+
+  data: () => ({}),
+};
 </script>
+
+<style>
+@import url("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css");
+</style>
